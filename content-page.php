5c19bc35d2675be19d7f6cdacda2f2945106a990
Patrick O'Hanlon <paddy@paddyohanlon.com>
2013-04-04 18:30:57 +0200

    Copy LSO theme to start working from

---
+++ content-page.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * The template used for displaying page content in page.php
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+?>
+
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+		<div class="entry-content">
+			<?php the_content(); ?>
+			<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'twentytwelve' ), 'after' => '</div>' ) ); ?>
+		</div><!-- .entry-content -->
+	</article><!-- #post -->
---
+++ footer.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains footer content and the closing of the
+ * #main and #page div elements.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+?>
+	</div><!-- #main .wrapper -->
+	
+</div><!-- #page -->
+
+<footer>
+  <small class="copyright">
+    <?php wp_nav_menu( array('menu' => 'Footer' )); ?>
+    <p>&copy; <?php echo date("Y") ?>. All right reserved.</p>
+  </small>
+</footer>
+
+
+<?php wp_footer(); ?>
+
+<script type="text/javascript">
+setTimeout(function(){var a=document.createElement("script");
+var b=document.getElementsByTagName("script")[0];
+a.src=document.location.protocol+"//dnn506yrbagrg.cloudfront.net/pages/scripts/0014/4272.js?"+Math.floor(new Date().getTime()/3600000);
+a.async=true;a.type="text/javascript";b.parentNode.insertBefore(a,b)}, 1);
+</script>
+
+</body>
+</html>
